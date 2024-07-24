d3cb0ca39bc94c1d8bc2fba8a950a6e300ef2675
sabaruheejagirdar <sabaruheejagirdar@gmail.com>
2024-07-23 21:38:15 -0500

    Two pointers: Three Sum

--- 2_TwoPointers/2_ThreeSum.py
+++ 2_TwoPointers/2_ThreeSum.py
@@ -8,27 +8,43 @@ Input: nums = [-1,0,1,2,-1,-4]
 Output: [[-1,-1,2],[-1,0,1]]
 
 Example2
-Input: nums = [0,1,1]
-Output: []
+Input: nums = [2,2,-4]
+Output: [[-4, 2, 2]]
+
 """
 """
 APPROACH:
+Follow the two-sum approach while keeping the current value constant in the loop.
+    threeSum = Current + left + right
+If Sum < 0: Increement left
+Sum > 0: Decreement Right.
+Handle duplicates to ensure unique triplets are considered.
 """
 
 def threeSum(nums): 
+    res = []
+    nums.sort()
 
-    for i in range(len(nums)):
-        while l < r:
-        l, r = i+1, len(nums)-1
-        while nums[l] == nums[i]:
-            l += 1
-        while nums[r] == nums[i]:
-            r -= 1
-        
-        whi
+    for idx, val in enumerate(nums):
+        if val == nums[idx-1]:
+            continue
 
+        left, right = idx+1, len(nums)-1
 
-    return False
+        while left < right:
+            threeSum = val + nums[left] + nums[right]
+
+            if threeSum< 0:
+                left += 1
+            elif threeSum > 0:
+                right -= 1
+            else:
+                res.append([val, nums[left], nums[right]])
+                left += 1
+                while nums[left] != nums[left-1] and left < right:
+                    left += 1
+    
+    return res
 
 nums = [-3,-3,1,2,3,4]
 result = threeSum(nums)
