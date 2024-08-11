a2cdb51255cf6372facfd6ece1c54e4f9a2bc70b
sabaruheejagirdar <sabaruheejagirdar@gmail.com>
2024-08-11 06:54:02 -0500

    Updated: Find Minimum in Rotated Sorted Array

--- 5_Binary Search/1_FindMinRotatedSortedArray.py
+++ 5_Binary Search/1_FindMinRotatedSortedArray.py
@@ -18,27 +18,37 @@ Output: 0
 APPROACH:
 Use binary search to find the minimum element in a rotated sorted array by comparing the middle element
 with the left and right boundaries, adjusting the search range accordingly.
+[3,4,5,1,2]
+- When left is smaller then right, its sorted array. Return left [3,4,5]
+- When middle is part of left sorted array, then smaller number is part of right sorted array.
+    nums[M] >= num[L]:
+        search smaller element at right side
+    else:
+        search smaller element at left side
 """
 
 def findMin(nums):   
-
-    left = 0
-    right = len(nums) - 1
     res = nums[0]
-    while left<= right:
-        if nums[left] < nums[right]:
-            res = min(res, nums[left])
+    L = 0
+    R = len(nums)-1
+
+    
+    while L <= R:
+        if nums[L] < nums[R]:
+            res = min(res,nums[L])
             break
+        
+        M = (L+R) //2
+        res = min(res, nums[M])
 
-        mid = (left + right)// 2
-        res = min(res, nums[mid])
-        if nums[mid] >= nums[left]:
-            left = mid + 1
+        if nums[M] >= nums[L]:
+            L = M + 1
         else:
-            right = mid -1
-
+            R = M - 1
+    
     return res
 
 nums = [3,4,5,1,2]
+# nums = [4,5,1,2,3]
 result = findMin(nums)
 print(result)
